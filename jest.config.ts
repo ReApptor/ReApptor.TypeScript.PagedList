bf8e6d67dfa2fec0afa7e3849aaa758e14772e60
Andrey Popov <61983961+YW5kcmV5@users.noreply.github.com>
2023-03-03 17:15:51 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+export default {
+  displayName: "ReApptor.TypeScript.PagedList",
+  preset: "../jest.preset.cjs",
+  coverageDirectory: "../coverage/libs/ReApptor.TypeScript.PagedList"
+};
