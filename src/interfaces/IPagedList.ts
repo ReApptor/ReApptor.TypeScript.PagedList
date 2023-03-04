1b54f8c1670698f1ba04dc83123bec645910d1c6
Andrey Popov <61983961+YW5kcmV5@users.noreply.github.com>
2023-03-04 19:30:21 +0200

    Comments and additional properties;

--- src/interfaces/IPagedList.ts
+++ src/interfaces/IPagedList.ts
@@ -1,11 +1,27 @@
 export default interface IPagedList<T = {}> {
+
+    /*
+     * The page items.
+     */
     items: T[];
 
+    /*
+     * The page index is in the superset starting from 1.
+     */
     pageNumber: number;
 
+    /*
+     * The maximum size of any page.
+     */
     pageSize: number;
 
+    /*
+     * The total number of pages within the superset
+     */
     pageCount: number;
 
+    /*
+     * The total number of elements contained within the superset.
+     */
     totalItemCount: number;
 }
--- src/models/PagedList.ts
+++ src/models/PagedList.ts
@@ -2,19 +2,37 @@ import IPagedList from "../interfaces/IPagedList";
 import PagedListUtility from "../utilities/PagedListUtility";
 
 export default class PagedList<T> implements IPagedList<T> {
-    
+
     public items: T[] = [];
-    
+
     public totalItemCount: number = 0;
 
     public pageNumber: number = 0;
 
     public pageSize: number = 0;
 
-    public get pageCount(): number {
-        return ((this.totalItemCount > 0) && (this.pageSize > 0)) ? ((this.totalItemCount - 1) / this.pageSize + 1) : 1;
+    public get hasPreviousPage(): boolean {
+        return (this.pageNumber > 1);
     }
-    
+
+    public get hasNextPage(): boolean {
+        return (this.pageNumber < this.pageCount);
+    }
+
+    public get isFirstPage(): boolean {
+        return (this.pageNumber == 1);
+    }
+
+    public get isLastPage(): boolean {
+        return (this.pageNumber >= this.pageCount);
+    }
+
+    public get pageCount(): number {
+        return ((this.totalItemCount > 0) && (this.pageSize > 0))
+            ? ((this.totalItemCount - 1) / this.pageSize + 1)
+            : 1;
+    }
+
     public isPagedList: true = true;
 
     public static toPagedList<T>(items: T[], pageNumber: number, pageSize: number): IPagedList<T> {
